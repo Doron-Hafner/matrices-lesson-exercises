8cd3be7bef89ba936f9e8cf8feb47b538eb8e38b
Doron Hafner <dh9707@gmail.com>
2020-09-25 17:45:41 +0300

    finish ex 1-9 need to do more extensions

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,12 +1,76 @@
-/* Write your code below */
+const matrix = require('./Matrix')
+class EmployeeMatrix extends matrix {
+    // constructor(rows, columns){
+    //     super(rows, columns)
+    // }
+    loadData (salaryData) {
+        this.matrix = []
+        salaryData.forEach(object => this.matrix.push((Object.values(object))))
+      }
+    getEmployees (department){
+        const names = []
+        this.matrix.forEach(row => {
+            row[2] === department ?
+            names.push(row[1]) : ''
+        });
+        return names
+      }
+    getTotalSalary (department) {
+    let sum = 0
+    this.matrix.forEach(row => {
+        row[2] === department ?
+        sum += row[3] : ''
+    });
+    return sum
+    }
+    findRichest() {
+        let maxSalary = {sal: 0}
+        this.matrix.forEach(row => {
+            if (row[3] > maxSalary.sal){
+                maxSalary.sal = row[3]
+                maxSalary.name = row[1]
+            }
+        })
+        return maxSalary.name
+    }
+}
+    
 
 
 
 
 //You can paste the code from the lesson below to test your solution
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
+// console.log(EmployeeMatrix);
+let m = new EmployeeMatrix()
+m.loadData(data)
+// console.log(m);
+m.print()
+//prints
 
+// e10021  Gillian Finance 2000
+// e10725  Tibor   Design  1200
+// e10041  Anisha  Finance 2300
+// e10411  Jakub   Design  1600
+// e11995  Mar     Design  1300
+// e10732  Nisha   Design  1200
+
+// console.log(m.getEmployees("Finance")) //prints [ 'Gillian', 'Anisha' ]
+// console.log(m.getEmployees("Design")) //prints [ 'Tibor', 'Jakub', 'Mar', 'Nisha' ]
+
+// console.log(m.getTotalSalary("Finance")) //prints 4300
+// console.log(m.getTotalSalary("Design")) //prints 5300
+
+// console.log(m.findRichest()) //prints Anisha
 
 
 
 /* Do not remove the exports below */
-module.exports = EmployeeMatrix
+// export default EmployeeMatrix
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,12 +1,77 @@
-/* Write your code below */
+class Matrix {
+    constructor (rows, columns) {
+        this.matrix = this.generateMatrix(rows, columns)
+    }
+    generateMatrix (rows, columns) {
+        let counter = 1
+        let matrix = []
+        for (let r = 0; r < rows; r++) {
+            matrix.push([])
+            for (let c = 0; c < columns; c++) {
+                matrix[r].push(counter ++)
+            }
+        }
+        return matrix
+    }
+    alter (row, col, num) {
+        this.matrix[row][col] = num
+    }
+    get (row, col){
+        return this.matrix[row][col]
+    }
+    // print =  function(){
+    //     for (let r = 0; r < this.matrix.length; r++) {
+    //             const str = this.matrix[r].toString().replace(/,/g,'   ')
+    //             console.log(str); 
+    //     }
+    // }
+    print () {
+        for (let i = 0; i < this.matrix.length; i++) {
+            let line = ""
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                line += (this.matrix[i][j] + "\t")
+            }
+            console.log(line)
+        }
+    }
+    printRow (rowNum){
+        for (let i = 0; i < this.matrix[rowNum].length; i++){
+            console.log(this.matrix[rowNum][i])
+        }
+    }
+    printColumn (columnNum){
+        for (let i = 0; i < this.matrix.length; i++) {
+            console.log(this.matrix[i][columnNum]); 
+        }
+    }
+    findCoordinate (value) {
+        const cord = {}
+        for (let i = 0; i < this.matrix.length; i++) {
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                if(this.matrix[i][j] === value){
+                    cord.x = j
+                    cord.y = i
+                }
+            }
+        }
+        return cord
+    }
+}
 
+let m = new Matrix(3, 4)
+// // console.log(m.matrix);
+// m.print()
+// //prints
+// /*
+// 1       2       3       4
+// 5       6       7       8
+// 9       10      11      12
+// */
 
+// m.alter(0, 0, m.get(1, 1))
+// m.printColumn(0) //prints 6, 5, 9 (separate lines)
+// // m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
+// console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+// console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 
-
-//You can paste the code from the lesson below to test your solution
-
-
-
-
-/* Do not remove the exports below */
 module.exports = Matrix
